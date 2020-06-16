b0a9963c5de3158438feb3c0d5645b3db3cf6c10
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2020-06-16 21:12:51 +0200

    #12: Updated copyright clauses.

--- src/main/java/com/github/squigglesql/squigglesql/criteria/MatchCriteria.java
+++ src/main/java/com/github/squigglesql/squigglesql/criteria/MatchCriteria.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2019 Joe Walnes, Guillaume Chauvet, Egor Nepomnyaschih.
+ * Copyright 2004-2020 Joe Walnes, Guillaume Chauvet, Egor Nepomnyaschih.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
